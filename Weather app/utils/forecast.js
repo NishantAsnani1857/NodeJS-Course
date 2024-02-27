9221803a905fc49771ac191093deeaf5640a354e
Nishant Asnani <nishant.a@crestinfosystems.com>
2024-02-27 14:10:02 +0530

    Ignoring .env Files

--- Weather app/utils/forecast.js
+++ Weather app/utils/forecast.js
@@ -1,11 +1,10 @@
 const request = require("postman-request");
-const apiKey1 = "7c65b08eca2f486abdd45926242002";
+const key=process.env.APIKEY1
 
 function getWeather(lat, long,callback) {
-  const api1 = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey1}&q=${lat},${long}&days=1&aqi=no&alerts=no`;
+  const api1 = `http://api.weatherapi.com/v1/forecast.json?key=${key}&q=${lat},${long}&days=1&aqi=no&alerts=no`;
 
   request({ url: api1, json: true }, (error, {body}) => {
-    // console.log(response.body);
     if (error) {
       console.log("Unable to fetch weather services for you");
     } else if (body.error) {
--- Web-Server/src/utils/geoCode.js
+++ Web-Server/src/utils/geoCode.js
@@ -1,8 +1,8 @@
 const request = require("postman-request");
-const apiKey2 = "65b8e1be7e354855668324oxgce1312";
+const key=process.env.APIKEY2
 
 function getCoordinates(location, callBack) {
-  const api2 = `https://geocode.maps.co/search?q=${location}&api_key=${apiKey2}`;
+  const api2 = `https://geocode.maps.co/search?q=${location}&api_key=${key}`;
   request({ url: api2, json: true }, (error, response) => {
     if (response === undefined) {
       console.log("Oh no some error occured");
